1521e77daf5bcf28ba508e7a6b4784c5a19edbfa
raikamv <149738264+raikamv@users.noreply.github.com>
2023-12-27 01:01:42 -0300

    quiz bíblico início

---
+++ script.js
@@ -0,0 +1,24 @@
+function salvar() {
+  const respostaPerguntaUm = document.querySelector(
+    'input[name="pergunta1"]:checked'
+  )?.value;
+
+  const respostaPerguntaDois = document.querySelector(
+    'input[name="pergunta2"]:checked'
+  )?.value;
+
+  let contador = 0;
+
+  if (respostaPerguntaUm === "66") {
+    contador = contador + 1;
+  }
+
+  if (respostaPerguntaDois === "galatas") {
+    contador = contador + 1;
+  }
+  debugger;
+
+  const divRespostas = document.getElementById("resultado");
+  divRespostas.innerHTML = "Total de acertos: " + contador;
+  divRespostas.setAttribute("style", "display: inherit");
+}
